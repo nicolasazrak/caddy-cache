0f40f256f7f80053d06fa991ae30855f3247bff6
Nicolas Azrak <azrak.nicolas@gmail.com>
2016-10-09 01:22:55 -0300

    Initial basic version

---
+++ cache.go
@@ -0,0 +1,126 @@
+package cache
+
+import (
+	"net/http"
+
+	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"github.com/mholt/caddy"
+	"net/http/httptest"
+	"fmt"
+	"gopkg.in/redis.v4"
+	"encoding/json"
+	"time"
+)
+
+type Rule struct {
+	Path    string
+}
+
+type CacheHandler struct {
+	Rules []Rule
+	Next  httpserver.Handler
+}
+
+func init() {
+	fmt.Println("Init!")
+	httpserver.RegisterDevDirective("cache", "root")
+	caddy.RegisterPlugin("cache", caddy.Plugin{
+		ServerType: "http",
+		Action:     Setup,
+	})
+}
+
+var client *redis.Client
+
+func Setup(c *caddy.Controller) error {
+	rules, err := parse(c)
+	if err != nil {
+		return err
+	}
+
+	c.OnStartup(func() error {
+		return nil
+	})
+
+	client = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+
+	cfg := httpserver.GetConfig(c)
+	mid := func(next httpserver.Handler) httpserver.Handler {
+		return CacheHandler{
+			Rules: rules,
+			Next: next,
+		}
+	}
+	cfg.AddMiddleware(mid)
+
+	return nil
+}
+
+
+func parse(c *caddy.Controller) ([]Rule, error) {
+	return []Rule{}, nil
+}
+
+type CachedResponse struct {
+	Code      int           // the HTTP response code from WriteHeader
+	Body      string
+	HeaderMap http.Header   // the HTTP response headers
+}
+
+
+func respond(cached * CachedResponse, w http.ResponseWriter, addCachedHeader bool) {
+	for k, vs := range cached.HeaderMap {
+		for _, v := range vs {
+			// fmt.Println("key=", k, "value=", v)
+			w.Header().Add(k, v)
+		}
+	}
+	if addCachedHeader {
+		w.Header().Add("cached", "1")
+	}
+	w.Write([]byte(cached.Body))
+	w.WriteHeader(cached.Code)
+}
+
+
+func (h CacheHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
+	val, err := client.Get(r.URL.Path).Result()
+	if err != nil && err != redis.Nil {
+		return 500, err
+	}
+
+	if err == redis.Nil {
+		rec := httptest.NewRecorder()
+		status, err := h.Next.ServeHTTP(rec, r)
+
+		cached := CachedResponse {
+			Body: string(rec.Body.Bytes()),
+			HeaderMap: rec.HeaderMap,
+			Code: rec.Code,
+		}
+
+		serialized, err := json.Marshal(cached)
+
+		if err != nil {
+			return 500, nil
+		}
+
+		err = client.Set(r.URL.Path, serialized, time.Duration(5) * time.Minute).Err()
+		if err != nil {
+			return 500, err
+		}
+
+		respond(&cached, w, false)
+		return status, err
+	} else {
+		cached := CachedResponse{}
+		json.Unmarshal([]byte(val), &cached)
+		respond(&cached, w, true)
+		return cached.Code, nil
+	}
+}
